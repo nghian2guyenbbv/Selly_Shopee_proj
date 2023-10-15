7aba71a3b21cb180954e4d725fd3b07b136bb8cd
nghianguyenbbv <nghia2nguyen@bbv.vn>
2023-10-15 19:57:08 +0700

    get Art from selly service last

--- docker-compose/getArticleFromSelly/getArtFromSelly/src/main/java/com/example/getartfromselly/controller/GetArtInfoController.java
+++ docker-compose/getArticleFromSelly/getArtFromSelly/src/main/java/com/example/getartfromselly/controller/GetArtInfoController.java
@@ -2,6 +2,8 @@ package com.example.getartfromselly.controller;
 
 import com.example.getartfromselly.article.ArticleInfoDto;
 import com.example.getartfromselly.article.request.GetArtWithKeyWordRequest;
+import com.example.getartfromselly.entity.ArticleSellyDto;
+import com.example.getartfromselly.repo.ArticleSellyRepository;
 import com.example.getartfromselly.service.GetArticleInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,9 +17,15 @@ public class GetArtInfoController {
     @Autowired
     private GetArticleInfoService getArticleInfoService;
 
+    @Autowired
+    private ArticleSellyRepository articleSellyRepository;
+
+
     @PostMapping("/getArtInfo")
     public ArticleInfoDto getArticleInfoWithKeyWord(@RequestBody GetArtWithKeyWordRequest getArtWithKeyWordRequest) {
         ArticleInfoDto artInfo = getArticleInfoService.getArticleInfoFromSelly(getArtWithKeyWordRequest.getKeyWord()).orElse(null);
+        ArticleSellyDto articleSellyDto = new ArticleSellyDto();
+        articleSellyRepository.save(articleSellyDto);
         return artInfo;
     }
 }
