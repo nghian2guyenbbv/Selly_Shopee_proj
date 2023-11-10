1538b590ba569fab58799fbb09da91c5592af7b0
nghianguyenbbv <nghia2nguyen@bbv.vn>
2023-11-10 20:27:50 +0700

    test create shopee art

---
+++ docker-compose/uploadShopeeArt/uploadShoppeArt/src/main/java/com/dropship/uploadshoppeart/article/KeyWord.java
@@ -0,0 +1,8 @@
+package com.dropship.uploadshoppeart.article;
+
+import lombok.Data;
+
+@Data
+public class KeyWord {
+  private String keyWord;
+}
--- docker-compose/uploadShopeeArt/uploadShoppeArt/src/main/java/com/dropship/uploadshoppeart/controller/CreateShoppeArt.java
+++ docker-compose/uploadShopeeArt/uploadShoppeArt/src/main/java/com/dropship/uploadshoppeart/controller/CreateShoppeArt.java
@@ -1,9 +1,11 @@
 package com.dropship.uploadshoppeart.controller;
 
+import com.dropship.uploadshoppeart.article.KeyWord;
 import com.dropship.uploadshoppeart.article.ShoppeArticle;
 import com.dropship.uploadshoppeart.service.ShopeeArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CreateShoppeArt {
   @Autowired
   private ShopeeArticleService shopeeArticleService;
-  @GetMapping("/create-article")
-  public void createShoppeArt(String artType){
-    shopeeArticleService.createArticle(artType);
+  @PostMapping("/create-article")
+  public void createShoppeArt(KeyWord artType){
+    shopeeArticleService.createArticle(artType.getKeyWord());
   }
 }
