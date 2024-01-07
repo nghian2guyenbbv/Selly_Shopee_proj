656b96aa013c1e7c398f12d0981ef23d50fa37c6
nghianguyenbbv <nghia2nguyen@bbv.vn>
2024-01-07 23:24:25 +0700

    Update cookie create product

--- docker-compose/uploadShopeeArt/uploadShoppeArt/src/main/java/com/dropship/uploadshoppeart/service/ShopeeArticleServiceImpl.java
+++ docker-compose/uploadShopeeArt/uploadShoppeArt/src/main/java/com/dropship/uploadshoppeart/service/ShopeeArticleServiceImpl.java
@@ -8,7 +8,6 @@ import com.dropship.uploadshoppeart.dto.ArticleSellyDto;
 import com.dropship.uploadshoppeart.repo.ArticleSellyRepo;
 import com.dropship.uploadshoppeart.request.CreateArtShopeeRq;
 import com.dropship.uploadshoppeart.response.CreateShopeeArtResponse;
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -17,13 +16,10 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
-import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -129,7 +125,13 @@ public class ShopeeArticleServiceImpl extends CommonClient implements ShopeeArti
   private HttpEntity getCreateArtEntity(ShoppeArticle shopeeArticle, List<String> baseCookie) {
     Assert.notNull(baseCookie, "base cookie must not be null");
     HttpHeaders header = getDefaultHeader();
-    header.set(COOKIE_PARAM, baseCookie.toString());
+    StringBuilder cookies = new StringBuilder();
+    for (String valueCookie : baseCookie) {
+      String cookie = valueCookie.replace("Secure", "").split(";")[0];
+      cookies.append(cookie).append(";");
+    }
+    System.out.println("cookies.toString() :"+cookies.toString());
+    header.set(HttpHeaders.COOKIE, cookies.toString());
     String request = CreateArtShopeeRq.tuiDeoNuRequest;
     String formatedRequest = request.replace("$ARTICLE_NAME", shopeeArticle.getArtName())
         .replace("$ARTICLE_DESCRIPTION", FormatmessageShopeeRule.formatMessage(shopeeArticle.getDescription()));
@@ -142,7 +144,7 @@ public class ShopeeArticleServiceImpl extends CommonClient implements ShopeeArti
     HttpHeaders header = getDefaultHeader();
     String ck = "REC_T_ID=e58e652b-ad24-11ee-8a90-baf7542d6a8b; SC_DFP=jhEvdTgxqcwLrZrCYoVAESytAvhtiANU; SPC_CLIENTID=bHpjVElrT2ozNmNswxaopnwpqscqttvn; SPC_EC=.a24zYlpraHRuSDRuSjFKONTOk2m2KelO71eLGJ6sT5xxAti2uYjZE6aVyNU7/stUWxe8kdvOiPalZTm0xb9S/3BUs+rCERXT9DIiFWEgCSAO3B4ferVuyUqEC3Q9wQD1j9IehuXnsUEdgrS3vDDpGzVB5B6J0mZwuf3cI9TWpLOFl8QoOArng0j6qNO5VAGdN+pzvRGqG0SrI8IAQEBC2Q==; SPC_F=lzcTIkOj36clNNPWTA0i02OHJJuyTLzC; SPC_R_T_ID=yxPikdxk8NBQxzjpHT+GmX1e02bueYSfVZ2KL+q1Hd0CPSeLQBhEKQ1bu9AdKvaLTsIecuOKFiHK7l+dCQ+lCdIWUT5f5VAMUr2dmdhhHwwF7riLEtgIs5XGE/kAy2KZTtgnhu+hxp5MJESq0nte3RUJQbQG3SDtzd7tNHZ1MRo=; SPC_R_T_IV=cTlSY2ZVajlkcWMwcVlmdA==; SPC_SC_TK=e28d1226c04e0e88e15b0b8fe05663cc; SPC_SC_UD=113891819; SPC_SI=1MqTZQAAAABMOTlaMnB3Wg8SVgAAAAAAdUd2VTdCU08=; SPC_ST=.a24zYlpraHRuSDRuSjFKONTOk2m2KelO71eLGJ6sT5xxAti2uYjZE6aVyNU7/stUWxe8kdvOiPalZTm0xb9S/3BUs+rCERXT9DIiFWEgCSAO3B4ferVuyUqEC3Q9wQD1j9IehuXnsUEdgrS3vDDpGzVB5B6J0mZwuf3cI9TWpLOFl8QoOArng0j6qNO5VAGdN+pzvRGqG0SrI8IAQEBC2Q==; SPC_STK=gqt28ULNKYQw5qNakoM/z0JcQEc11X5k9eEPEivt2asVX7yaHEnPcUS/WBgJsvtUzFZ2VH1S+bsaEdsSTqi8xskmY4/Wg4U3u2CuIX2+hCgfWflBadK3t2PvAohFjT7rMjkxdBvXLN5GawLp7z3tnVUfQQoA7yLsnOq81sAXiCG+WsqVD3flnPPGm5PUDeWU; SPC_T_ID=yxPikdxk8NBQxzjpHT+GmX1e02bueYSfVZ2KL+q1Hd0CPSeLQBhEKQ1bu9AdKvaLTsIecuOKFiHK7l+dCQ+lCdIWUT5f5VAMUr2dmdhhHwwF7riLEtgIs5XGE/kAy2KZTtgnhu+hxp5MJESq0nte3RUJQbQG3SDtzd7tNHZ1MRo=; SPC_T_IV=cTlSY2ZVajlkcWMwcVlmdA==";
     //header.set(HttpHeaders.COOKIE, CollectionUtils.isEmpty(baseCookie) ? Strings.EMPTY : baseCookie.toString());
-        header.set(HttpHeaders.COOKIE, StringUtils.arrayToDelimitedString(baseCookie.toArray(), ";"));
+    header.set(HttpHeaders.COOKIE, StringUtils.arrayToDelimitedString(baseCookie.toArray(), ";"));
     header.set("referer", "https://banhang.shopee.vn/?is_from_login=true");
     header.set("authority", "banhang.shopee.vn");
     return new HttpEntity<>(header);
