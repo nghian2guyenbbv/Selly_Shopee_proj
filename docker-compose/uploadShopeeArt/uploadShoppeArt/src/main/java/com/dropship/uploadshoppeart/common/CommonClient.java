46422e82898192e9a8c05679ab13f0d9a12a4c24
nghianguyenbbv <nghia2nguyen@bbv.vn>
2024-01-07 14:01:17 +0700

    upload articles to shoppe with cookie base

--- docker-compose/uploadShopeeArt/uploadShoppeArt/src/main/java/com/dropship/uploadshoppeart/common/CommonClient.java
+++ docker-compose/uploadShopeeArt/uploadShoppeArt/src/main/java/com/dropship/uploadshoppeart/common/CommonClient.java
@@ -1,10 +1,26 @@
 package com.dropship.uploadshoppeart.common;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public class CommonClient {
-  public HttpHeaders getDefaultHeader(){
+
+  public static final String COOKIE_PARAM = "cookie";
+
+  public List<String> getSetCookieResponse(ResponseEntity responseEntity) {
+    return responseEntity.getHeaders().get(HttpHeaders.SET_COOKIE);
+  }
+
+  public HttpHeaders getDefaultHeader() {
     // Default header
     final HttpHeaders httpHeaders = new HttpHeaders();
     httpHeaders.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
