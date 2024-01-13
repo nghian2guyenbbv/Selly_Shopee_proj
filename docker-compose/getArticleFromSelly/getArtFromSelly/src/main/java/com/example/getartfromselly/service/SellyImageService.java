b306f0c6bee8b2132047469cf551fa002490ebf0
nghianguyenbbv <nghia2nguyen@bbv.vn>
2024-01-13 18:09:26 +0700

    init selly images service

---
+++ docker-compose/getArticleFromSelly/getArtFromSelly/src/main/java/com/example/getartfromselly/service/SellyImageService.java
@@ -0,0 +1,10 @@
+package com.example.getartfromselly.service;
+
+import com.example.getartfromselly.common.ShopeeImageName;
+
+import java.util.List;
+
+public interface SellyImageService {
+  List<ShopeeImageName> downloadImagesWithUrl(String productName);
+  void saveShopeeImageName(List<ShopeeImageName> shopeeImageNames);
+}
---
+++ docker-compose/getArticleFromSelly/getArtFromSelly/src/main/java/com/example/getartfromselly/service/SellyImageServiceImpl.java
@@ -0,0 +1,17 @@
+package com.example.getartfromselly.service;
+
+import com.example.getartfromselly.common.ShopeeImageName;
+
+import java.util.List;
+
+public class SellyImageServiceImpl implements SellyImageService{
+  @Override
+  public List<ShopeeImageName> downloadImagesWithUrl(String productName) {
+    return null;
+  }
+
+  @Override
+  public void saveShopeeImageName(List<ShopeeImageName> shopeeImageNames) {
+
+  }
+}
